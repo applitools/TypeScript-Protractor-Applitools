7318a60e7dc075e813c3e37a1727f02cde9d6496
Manoj Kumar <manoj9788@gmail.com>
2018-05-01 11:49:18 +0530

    nicer js es format

--- specs/FirstSpec.ts
+++ specs/FirstSpec.ts
@@ -2,21 +2,21 @@ import { ElementFinder, browser, By, element } from 'protractor';
 import {Eyes, ConsoleLogHandler, StitchMode} from 'eyes.selenium';
 
 let eyes = null;
-beforeAll(function(){
+beforeAll(() => {
     eyes = new Eyes();
-    eyes.setApiKey("Your_API_key");
+    eyes.setApiKey("HZeTQj103qqbLJkpT0Ez81CTbivkDEYQx4Q7GjM104Kv2Hg110");
     //eyes.setLogHandler(new ConsoleLogHandler(true));
     //eyes.setStitchMode(StitchMode.CSS);
 });
-beforeEach(function(done){
-    eyes.open(browser, "TypeScript-Example", "NewTest", {width: 800, height: 600}).then(function () {
+beforeEach(done => {
+    eyes.open(browser, "TypeScript-Example", "NewTest", {width: 800, height: 600}).then(() => {
         done();
     });
 });
 
-describe('angularjs homepage todo list', function () { //Suite in Jasmine
+describe('angularjs homepage todo list', () => { //Suite in Jasmine
 
-    it('should add a todo', function () { // Test in Jasmine
+    it('should add a todo', () => { // Test in Jasmine
         // Entering application url in browser
         browser.get('https://angularjs.org');
 
@@ -29,10 +29,10 @@ describe('angularjs homepage todo list', function () { //Suite in Jasmine
         eyes.checkWindow("Entire Nxt window");
 
         element(By.css('[value="add"]')).click(); // Clicks on 'Add' button
-        element.all(By.repeater('todo in')).then(function (todoList) {
+        element.all(By.repeater('todo in')).then(todoList => {
             // Asserting the TODO's count as 3
             expect(todoList.length.toString()).toEqual('3');
-            todoList[2].getText().then(function (text) {
+            todoList[2].getText().then(text => {
                 //Verifying newly entered TODO is added
                 expect(text).toEqual('write first protractor test');
             });
@@ -40,14 +40,14 @@ describe('angularjs homepage todo list', function () { //Suite in Jasmine
     });
 });
 
-afterEach(function(done) {
-    eyes.close().then(function () {
+afterEach(done => {
+    eyes.close().then(() => {
         done();
     });
 });
 
-afterAll(function(done) {
-    eyes.abortIfNotClosed().then(function () {
+afterAll(done => {
+    eyes.abortIfNotClosed().then(() => {
         done();
     });
 });
